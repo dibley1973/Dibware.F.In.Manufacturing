1240546a08e97dea1ffb09e702dd14ed9e1b7f06
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-25 20:28:59 +0000

    Add SmeltingTests

---
+++ src/Dibware.F.In.Manufacturing.Domain.UnitTests/SmeltingTests.fs
@@ -0,0 +1,34 @@
+﻿namespace Dibware.F.In.Manufacturing.Domain.UnitTests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Dibware.F.In.Manufacturing.Domain.Plants
+open Dibware.F.In.Manufacturing.Domain.Data
+
+[<TestClass>]
+type SmeltingTests () =
+
+    [<TestMethod>]
+    member this.getSmeltingPlantForOre_WithSupportedOre_ReturnsSmelter () =
+        // Arrange
+        let expected = SmeltingPlant.IronOreSmelter
+        let ironOre = Ore.IronOre 
+
+        // Act
+        let actual = Smelting.getSmeltingPlantForOre(ironOre)
+
+        // Assert
+        Assert.IsTrue(actual.IsSome);
+        Assert.AreEqual((expected |> Some), actual);
+
+    [<TestMethod>]
+    member this.getSmeltingPlantForOre_WithunsupportedOre_ReturnsNone () =
+        // Arrange
+        //let expected = SmeltingPlant.IronOreSmelter
+        let ironOre = Ore.GoldOre
+
+        // Act
+        let actual = Smelting.getSmeltingPlantForOre(ironOre)
+
+        // Assert
+        Assert.IsTrue(actual.IsNone);
