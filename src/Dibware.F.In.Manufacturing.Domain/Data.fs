f3c3e4227f9a5b2c4aa852415c52e9fff4291c3e
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-25 16:40:59 +0000

    Rename `Library.fs` to `Data.fs` and add data structures

---
+++ src/Dibware.F.In.Manufacturing.Domain/Data.fs
@@ -0,0 +1,59 @@
+﻿namespace Dibware.F.In.Manufacturing.Domain.Data
+
+/// <summary>
+/// Describes a raw, unprocessed material used in manufacturing.
+/// </summary>
+type RawMaterial = {
+    /// <summary>
+    /// The name of the material
+    /// </summary>
+    Name: string
+}
+
+/// <summary>
+/// Describes the type of raw, unprocessed material
+/// </summary>
+type RawMaterialType =
+    | Ore
+    | Oil
+    | Gas
+    | Lumber
+
+/// <summary>
+/// Describes a material that can be used in manufacturing, which can be either raw or processed / refined.
+/// </summary>
+type ProcessedMaterial = {
+    Name: string
+}
+
+/// <summary>
+/// Describes a component manufacturedfrom processed materials.
+/// </summary>
+type BasicComponent = {
+    Name: string
+    Materials: ProcessedMaterial list
+}
+
+type AssembledComponent = {
+    Name: string
+    SubComponents: BasicComponent list
+}
+
+type Component =
+    | Basic of BasicComponent
+    | Assembled of AssembledComponent
+
+
+type Product = {
+    Name: string
+    Components: Component list
+}
+
+
+
+
+type MaterialType =
+    | Metal
+    | Plastic
+    | Wood
+    | Composite
--- src/Dibware.F.In.Manufacturing.Domain/Library.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace Dibware.F.In.Manufacturing.Domain
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
