bb8d0bf9682256e8f8aac549d7cee968d819302f
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-26 21:32:16 +0000

    Add more Rocks Update Mines

--- src/Dibware.F.In.Manufacturing.Domain/Types/Mining.fs
+++ src/Dibware.F.In.Manufacturing.Domain/Types/Mining.fs
@@ -4,10 +4,14 @@ namespace Dibware.F.In.Manufacturing.Domain.Types.Mining
 /// Describes different types of rocks that contain ores.
 /// </summary>
 type Rock = 
-    | IronImpregnatedRock
-    | CoalimpregnatedRock
-    | CopperImpregnatedRock
-    | Shale
+    | IronImpregnatedRock   // Contains iron ore
+    | CoalimpregnatedRock   // Contains coal
+    | CopperImpregnatedRock // Contains copper ore
+    | GasShale              // Sedimentary rock that contains natural gas
+    | OilShale              // Sedimentary rock that contains oil 
+    | Spoil                 // Waste rock removed during mining
+    | Useless               // Rock that does not contain any ore.or substance of value
+    | VoidOfAnyRock         // Absence of rock, e.g., in a tunnel or cavern, or where rock has been already mined
 
 /// <summary>
 /// Describes different types of ores used as raw materials.
@@ -24,6 +28,5 @@ type Ore =
 /// Describes a mining operation that extracts raw materials from the earth.
 /// </summary>
 type Mine = 
-    | CoalMine
-    | CopperMine
-    | IronOreMine
+    | OpenPitMine
+    | UndergroundShaftMine
