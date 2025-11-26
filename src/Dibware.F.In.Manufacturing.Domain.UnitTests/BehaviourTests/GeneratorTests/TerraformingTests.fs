47e1825638437d847e5a8a70b25e88fa0b32e1db
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-26 05:37:44 +0000

    Add `terraformBiasedLand_With70PercentPreferredRock_ReturnsGameAreaContainsAtLeast70OfPreferredRockElements` test and correct syntax for `TerraformingTests` to get tests to run properly.

--- src/Dibware.F.In.Manufacturing.Domain.UnitTests/BehaviourTests/GeneratorTests/TerraformingTests.fs
+++ src/Dibware.F.In.Manufacturing.Domain.UnitTests/BehaviourTests/GeneratorTests/TerraformingTests.fs
@@ -5,7 +5,7 @@ open Dibware.F.In.Manufacturing.Domain.Behaviours.Generators.Terraforming
 open Dibware.F.In.Manufacturing.Domain.Types.Mining
 
 [<TestClass>]
-type TerraformingTests =
+type TerraformingTests () =
 
     [<TestMethod>]
     member public this.terraformRandomLand_WithValidDimensions_ReturnsGameArea () =
@@ -34,3 +34,22 @@ type TerraformingTests =
         Assert.IsNotNull(actual)
         Assert.AreEqual(length, actual.Grid.GetLength(0))
         Assert.AreEqual(width, actual.Grid.GetLength(1))
+
+    [<TestMethod>]
+    member public this.terraformBiasedLand_With70PercentPreferredRock_ReturnsGameAreaContainsAtLeast70OfPreferredRockElements () =
+        // Arrange
+        let length = 10
+        let width = 10
+        let preferredRock = Rock.IronImpregnatedRock
+        let expectedFlattenedLength = length * width
+        let expectedMinimumPreferredRockCount = (expectedFlattenedLength * 70) / 100
+
+        // Act
+        let landGrid = terraformBiasedLand(length, width, 70) Rock.IronImpregnatedRock
+        let actual = landGrid.Grid |> Seq.cast<Rock> |> Seq.toArray
+        let actualPreferredRockCount = actual |> Array.filter(fun rock -> rock = preferredRock) |> Array.length
+        
+        // Assert
+        Assert.IsNotNull(actual)
+        Assert.AreEqual(expectedFlattenedLength, actual.GetLength(0))
+        Assert.IsTrue(actualPreferredRockCount >= expectedMinimumPreferredRockCount)
