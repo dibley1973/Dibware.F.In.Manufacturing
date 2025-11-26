bef567bbd5b338a3d5c9ed6e1eb70be3c3e24b62
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-26 20:17:13 +0000

    Add Inventory

---
+++ src/Dibware.F.In.Manufacturing.Domain/Types/Inventory.fs
@@ -0,0 +1,11 @@
+﻿namespace Dibware.F.In.Manufacturing.Domain.Types
+
+open Dibware.F.In.Manufacturing.Domain.Types.Mining
+open Dibware.F.In.Manufacturing.Domain.Types.Materials
+
+type InventoryItem =
+    | Rock of Rock
+    | Ore of Ore
+    | Raw of RawMaterial
+    | Processed of ProcessedMaterial
+    | ComponentItem of Component
