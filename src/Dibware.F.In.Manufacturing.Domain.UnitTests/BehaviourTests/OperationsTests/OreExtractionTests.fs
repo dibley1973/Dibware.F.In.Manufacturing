6a50b5d866e70f83fd2ea6673dcaa506c72180e0
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-27 20:33:03 +0000

    Add `OreExtraction` operations and tests for them.

---
+++ src/Dibware.F.In.Manufacturing.Domain.UnitTests/BehaviourTests/OperationsTests/OreExtractionTests.fs
@@ -0,0 +1,49 @@
+﻿namespace Dibware.F.In.Manufacturing.Domain.UnitTests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Dibware.F.In.Manufacturing.Domain.Behaviours.Operations
+open Dibware.F.In.Manufacturing.Domain.Types.Mining
+
+/// <summary>
+/// Tests for the Ore Extraction operations.
+/// </summary>
+[<TestClass>]
+type  OreExtractionTests ()=
+    [<TestMethod>]
+    member this.ExtractOre_WithSupportedRock_ReturnsSomeIronOre() =
+        // Arrange
+        let expected = (Ore.IronOre |> Some)
+        let rock = Rock.IronImpregnatedRock
+
+        // Act
+        let actual = OreExtraction.ExtractOre(rock)
+
+        // Assert
+        Assert.IsTrue(actual.IsSome);
+        Assert.AreEqual(expected, actual);
+
+    [<TestMethod>]
+    member this.ExtractOre_WithUselessRock_ReturnsNone() =
+        // Arrange
+        let expected = None
+        let rock = Rock.Useless
+
+        // Act
+        let actual = OreExtraction.ExtractOre(rock)
+
+        // Assert
+        Assert.IsTrue(actual.IsNone);
+        Assert.AreEqual(expected, actual);
+
+    [<TestMethod>]
+    member this.ExtractOre_WithVoidOfRock_ReturnsNone() =
+        // Arrange
+        let expected = None
+        let rock = Rock.VoidOfAnyRock
+
+        // Act
+        let actual = OreExtraction.ExtractOre(rock)
+
+        // Assert
+        Assert.IsTrue(actual.IsNone);
+        Assert.AreEqual(expected, actual);
---
+++ src/Dibware.F.In.Manufacturing.Domain/Behaviours/Operations/OreExtraction.fs
@@ -0,0 +1,20 @@
+﻿namespace Dibware.F.In.Manufacturing.Domain.Behaviours.Operations
+
+open Dibware.F.In.Manufacturing.Domain.Types.Mining
+
+/// <summary>
+/// Describes operations related to mining plants.
+/// </summary>
+module OreExtraction =
+    /// <summary>
+    /// Processes the supplied rock to extract ore, if any.
+    /// </summary>
+    /// <param name="rock">The rock to be proessed.</param>
+    /// <returns>The extracted ore, or None if no ore is present.</returns>
+    let public ExtractOre (rock: Rock) : Ore option =
+        match rock with
+        | Rock.IronImpregnatedRock -> Ore.IronOre |> Some
+        | Rock.CoalImpregnatedRock -> Ore.Coal |> Some
+        | Rock.CopperImpregnatedRock -> Ore.CopperOre |> Some
+        | Rock.Useless -> None
+        | _ -> None
