3dda167d84ee5ded2724c7ed39d770daf89cc3dc
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-28 22:05:27 +0000

    Add `Recipe`

---
+++ src/Dibware.F.In.Manufacturing.Domain/Types/Manufacturing.fs
@@ -0,0 +1,14 @@
+﻿namespace Dibware.F.In.Manufacturing.Domain.Types.Manufacturing
+
+open Dibware.F.In.Manufacturing.Domain.Types.Materials
+
+/// <summary>
+/// Describes a manufacturing recipe, defining the input materials,
+/// output materials, and time required for the manufacturing process.
+/// </summary>
+type Recipe = {
+    Input: Map<Material, int>
+    Output: Map<Material, int>
+    Time: float
+}
+
