514db30c2cac00fbec7a28dbc81cb71a1d34c4df
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-28 20:09:45 +0000

    Add `Surveyiong` and tests to prove

---
+++ src/Dibware.F.In.Manufacturing.Domain.UnitTests/BehaviourTests/OperationsTests/SurveyingTests.fs
@@ -0,0 +1,42 @@
+﻿namespace Dibware.F.In.Manufacturing.Domain.UnitTests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Dibware.F.In.Manufacturing.Domain.Behaviours.Operations
+open Dibware.F.In.Manufacturing.Domain.Types.Mining
+open Dibware.F.In.Manufacturing.Domain.Types.Terraforming
+open Dibware.F.In.Manufacturing.Domain.Types.Measurements
+
+/// <summary>
+/// Tests for Surveying behaviour
+/// </summary>
+[<TestClass>]
+type SurveyingTests () =
+    [<TestMethod>]
+    member this.surveyLocation_WithRockPresent_ReturnsRock () =
+        // Arrange
+        let location : Coordinate2D = { XPosition = 0; YPosition = 0}
+        let rockMap = Array2D.create 1 1 Rock.IronImpregnatedRock
+        let worldSize : Size2D = { XLength = 1; YLength = 1 }
+        let world : World2D = { Dimensions = worldSize; Map = rockMap }
+        
+        // Act
+        let actual = Surveying.surveyLocation(location, world)
+        
+        // Assert
+        Assert.IsTrue(actual.IsSome)
+        Assert.AreEqual(Rock.IronImpregnatedRock, actual.Value)
+
+    [<TestMethod>]
+    member this.surveyLocation_WithNoRockPresent_ReturnsNone () =
+        // Arrange
+        let location : Coordinate2D = { XPosition = 0; YPosition = 0}
+        let rockMap = Array2D.create 1 1 Rock.VoidOfAnyRock
+        let worldSize : Size2D = { XLength = 1; YLength = 1 }
+        let world : World2D = { Dimensions = worldSize; Map = rockMap }
+        
+        // Act
+        let actual = Surveying.surveyLocation(location, world)
+        
+        // Assert
+        Assert.IsTrue(actual.IsNone)
+        Assert.AreEqual(None, actual)
---
+++ src/Dibware.F.In.Manufacturing.Domain/Behaviours/Operations/Surveying.fs
@@ -0,0 +1,25 @@
+﻿namespace Dibware.F.In.Manufacturing.Domain.Behaviours.Operations
+
+open Dibware.F.In.Manufacturing.Domain.Types.Mining
+open Dibware.F.In.Manufacturing.Domain.Types.Measurements
+open Dibware.F.In.Manufacturing.Domain.Types.Terraforming
+
+/// <summary>
+/// Describes operations related to surveying locations in the game world.
+/// </summary>
+module Surveying =
+    /// <summary>
+    /// A function that performs a surveying operation to identify 
+    /// rock types in the game world.
+    /// </summary>
+    /// <param name="location">The 2D location to check for rock.</param>
+    /// <param name="world">The 2D world containing the rock map.</param>
+    /// <returns>
+    /// The rock at the specified location, or None if no rock is present.
+    /// </returns>
+    let public surveyLocation (location: Coordinate2D, world: World2D) : Rock option =
+        let rockAtLocation = world.Map.[location.XPosition, location.YPosition]
+        
+        match rockAtLocation with
+        | Rock.VoidOfAnyRock -> None
+        | _ -> (rockAtLocation |> Some)
