ab8145fdd83cc555eb5a24637912421905ee8200
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-29 19:43:37 +0000

    Update `Mining`

--- src/Dibware.F.In.Manufacturing.Domain/Types/Mining.fs
+++ src/Dibware.F.In.Manufacturing.Domain/Types/Mining.fs
@@ -1,28 +1,30 @@
 namespace Dibware.F.In.Manufacturing.Domain.Types.Mining
 
+open Dibware.F.In.Manufacturing.Domain.Types.Materials
+
 /// <summary>
 /// Describes different types of rocks that contain ores.
 /// </summary>
 type Rock = 
-    | IronImpregnatedRock   // Contains iron ore
-    | CoalImpregnatedRock   // Contains coal
-    | CopperImpregnatedRock // Contains copper ore
-    | GasShale              // Sedimentary rock that contains natural gas
-    | OilShale              // Sedimentary rock that contains oil 
-    | Spoil                 // Waste rock removed during mining
-    | Useless               // Rock that does not contain any ore.or substance of value
-    | VoidOfAnyRock         // Absence of rock, e.g., in a tunnel or cavern, or where rock has been already mined
+    | IronImpregnatedRock   //of Material // Contains iron ore
+    | CoalImpregnatedRock   //of Material // Contains coal
+    | CopperImpregnatedRock //of Material // Contains copper ore
+    | GasShale              //of Material // Sedimentary rock that contains natural gas
+    | OilShale              //of Material // Sedimentary rock that contains oil 
+    | Spoil                 //of Material // Waste rock removed during mining
+    | Useless               //of Material // Rock that does not contain any ore.or substance of value
+    | VoidOfAnyRock         //of Material // Absence of rock, e.g., in a tunnel or cavern, or where rock has been already mined
 
 /// <summary>
 /// Describes different types of ores used as raw materials.
 /// </summary>
-type Ore = 
-    | IronOre
-    | Coal
-    | CopperOre
-    | AluminumOre
-    | GoldOre
-    | SilverOre
+type Ore =
+    | IronOre     // of Ore 
+    | Coal        //of Material 
+    | CopperOre   //of Material 
+    | AluminumOre //of Material 
+    | GoldOre     //of Material
+    | SilverOre   //of Material 
 
 /// <summary>
 /// Describes a mining operation that extracts raw materials from the earth.
