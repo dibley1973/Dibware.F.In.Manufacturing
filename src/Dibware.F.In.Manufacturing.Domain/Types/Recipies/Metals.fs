136c8444b2cd0d301d4d01112fb995924cf333ce
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-29 19:46:07 +0000

    Add empty Alloying recipy in `Metals`

---
+++ src/Dibware.F.In.Manufacturing.Domain/Types/Recipies/Metals.fs
@@ -0,0 +1,13 @@
+namespace Dibware.F.In.Manufacturing.Domain.Types.Recipies.Metals
+
+//open Dibware.F.In.Manufacturing.Domain.Types.Materials
+//open Dibware.F.In.Manufacturing.Domain.Types.Manufacturing
+
+//module Alloying =
+//    let makeSteel : Recipe = {
+//        Input = Map.ofList [ (SteelIngot, 2) ]
+//        Output = Map.ofList [ (Coal, 1) ]
+//        Time = 5.0 // seconds
+//    }
+
+
