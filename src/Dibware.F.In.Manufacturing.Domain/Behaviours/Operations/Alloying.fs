b03949f4a7939e8978372de615782f39466c7474
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-29 19:46:37 +0000

    Add empty `Alloying` operation

---
+++ src/Dibware.F.In.Manufacturing.Domain/Behaviours/Operations/Alloying.fs
@@ -0,0 +1,13 @@
+namespace Dibware.F.In.Manufacturing.Domain.Behaviours.Operations
+
+open Dibware.F.In.Manufacturing.Domain.Types.Materials
+open Dibware.F.In.Manufacturing.Domain.Types.Manufacturing
+
+//module Alloying =
+//    let makeSteel : Recipe = {
+//        Input = Map.ofList<Material, int> [ (SteelIngot, 2) ]
+//        Output = Map.ofList<Material, int> [ (O2.Coal2, 1) ]
+//        Time = 5.0 // seconds
+//    }
+
+
