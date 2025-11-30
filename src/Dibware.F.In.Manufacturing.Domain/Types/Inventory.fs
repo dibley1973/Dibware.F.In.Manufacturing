1a5c99f2987b5c387d63fb0ed5f6a6e3d9ba3ad4
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-30 20:31:42 +0000

    Comment out original modules for materials and inventory

--- src/Dibware.F.In.Manufacturing.Domain/Types/Inventory.fs
+++ src/Dibware.F.In.Manufacturing.Domain/Types/Inventory.fs
@@ -1,33 +1,48 @@
 namespace Dibware.F.In.Manufacturing.Domain.Types.Inventory
 
-open Dibware.F.In.Manufacturing.Domain.Types.Mining
-open Dibware.F.In.Manufacturing.Domain.Types.Materials
+module DeleteMeLater =
+    let x = 42
+    (*
+    This module is to be deleted later.
+    It contains an inventory implementation that uses
+    my original material type definitions, rather than
+    the alternative material type definitions provided
+    by Brian Berns.
+    *)
 
-/// <summary>
-/// Represents the name of an item in the inventory.
-/// </summary>
-type Name = string
+///// <summary>
+///// Using my original material type definitions.
+///// </summary>
+//module Inventory_OriginalMaterialTypes =
 
-/// <summary>
-/// Represents the quantity of a specific item in an inventory.
-/// </summary>
-type Quantity = int
+//open Dibware.F.In.Manufacturing.Domain.Types.Mining
+//open Dibware.F.In.Manufacturing.Domain.Types.Materials
 
-/// <summary>
-/// Represents a type of an item in the inventory.
-/// </summary>
-type InventoryItem =
-    | Rock of Rock
-    | Ore of Ore
-    | Raw of RawMaterial
-    | Processed of ProcessedMaterial
-    | ComponentItem of Component
+///// <summary>
+///// Represents the name of an item in the inventory.
+///// </summary>
+//type Name = string
 
-/// <summary>
-/// Represents an inventory item along with its quantity.
-/// </summary>
-type InventoryItemQuantity = {
-    Name : Name
-    Item : InventoryItem
-    Quantity : Quantity
-}
+///// <summary>
+///// Represents the quantity of a specific item in an inventory.
+///// </summary>
+//type Quantity = int
+
+///// <summary>
+///// Represents a type of an item in the inventory.
+///// </summary>
+//type InventoryItem =
+//    | Rock of Rock
+//    | Ore of Ore
+//    | Raw of RawMaterial
+//    | Processed of ProcessedMaterial
+//    | ComponentItem of Component
+
+///// <summary>
+///// Represents an inventory item along with its quantity.
+///// </summary>
+//type InventoryItemQuantity = {
+//    Name : Name
+//    Item : InventoryItem
+//    Quantity : Quantity
+//}
--- src/Dibware.F.In.Manufacturing.Domain/Types/Materials.fs
+++ src/Dibware.F.In.Manufacturing.Domain/Types/Materials.fs
@@ -1,114 +1,95 @@
 namespace Dibware.F.In.Manufacturing.Domain.Types.Materials
 
-/// <summary>
-/// Describes the base for any material used in manufacturing.
-/// </summary>
-type Material = {
-    /// <summary>
-    /// The name of the material
-    /// </summary>
-    Name: string
-}
-
-//type Mat(name: string) 
-//    
-//type Rmat() =
-//    inherit Mat()
-
-//// Level 2
-//type Raw2Material = Raw2Material of Material
-//type Processed2Material = Processed2Material of Material
-//type Rock2 = Rock2 of Raw2Material
-
-//type Timber2 = Timber2 of Raw2Material
-
-//type Ore2 = Ore2 of Raw2Material
-//type Oil2 = Oil2 of Raw2Material
-//type Gas2 = Gas2 of Raw2Material
-//type IronOre2 = IronOre2 of Ore2
-//type Coal2 = Coal2 of Ore2
-
-//type O2 =
-//    | IronOre2 of Ore2
-//    | Coal2 of Ore2
-//    | CopperOre2 of Ore2
-
-
-/// <summary>
-/// Describes the type of raw, unprocessed material
-/// </summary>
-type RawMaterial =
-    | Ore    //of Material
-        //| IronOre //of Material
-        //| Coal    //of Material
-        //| CopperOre of Material
-        //| AluminumOre of Material
-        //| GoldOre of Material
-        //| SilverOre of Material
-    | Oil    of Material
-        | CrudeOil of Material
-    | Gas    of Material
-        | NaturalGas of Material
-    | Timber of Material
-        | Logs of Material
-
-/// <summary>
-/// Describes the type of processed / refined material
-/// </summary>
-type PrimaryMaterialType =
-    | MetalIngot of Material
-    | PlasticPellets of Material
-    | Logs of Material
-    | CompositeFibers of Material
-
-/// <summary>
-/// Describes plant that Smelts ores into processed materials.
-/// </summary>
-type SmeltingPlant = 
-    | IronOreSmelter
+module DeleteMeLater =
+    let x = 42
+(*
+This module is to be deleted later.
+It contains material type definitions that
+are to be replaced by the alternative
+material type definitions provided
+by Brian Berns.
+*)
 
 ///// <summary>
-///// Describes a material that can be used in manufacturing, which can be either raw or processed / refined.
+///// Describes the base for any material used in manufacturing.
 ///// </summary>
-//type Material = { Name : string }
+//type Material = {
+//    /// <summary>
+//    /// The name of the material
+//    /// </summary>
+//    Name: string
+//}
 
-/// <summary>
-/// Describes a processed material used in manufacturing.
-/// </summary>
-type ProcessedMaterial =
-    | Iron of Material
-    | Steel of Material
-    | Plastic of Material
+///// <summary>
+///// Describes the type of raw, unprocessed material
+///// </summary>
+//type RawMaterial =
+//    | Ore    //of Material
+//        //| IronOre //of Material
+//        //| Coal    //of Material
+//        //| CopperOre of Material
+//        //| AluminumOre of Material
+//        //| GoldOre of Material
+//        //| SilverOre of Material
+//    | Oil    of Material
+//        | CrudeOil of Material
+//    | Gas    of Material
+//        | NaturalGas of Material
+//    | Timber of Material
+//        | Logs of Material
 
-/// <summary>
-/// Describes a component manufacturedfrom processed materials.
-/// </summary>
-type BasicComponent = {
-    Name: string
-    Materials: ProcessedMaterial list
-}
+///// <summary>
+///// Describes the type of processed / refined material
+///// </summary>
+//type PrimaryMaterialType =
+//    | MetalIngot of Material
+//    | PlasticPellets of Material
+//    | Logs of Material
+//    | CompositeFibers of Material
 
-type AssembledComponent = {
-    Name: string
-    SubComponents: BasicComponent list
-}
+///// <summary>
+///// Describes plant that Smelts ores into processed materials.
+///// </summary>
+//type SmeltingPlant = 
+//    | IronOreSmelter
 
-type Component =
-    | Basic of BasicComponent
-    | Assembled of AssembledComponent
+///// <summary>
+///// Describes a processed material used in manufacturing.
+///// </summary>
+//type ProcessedMaterial =
+//    | Iron of Material
+//    | Steel of Material
+//    | Plastic of Material
+
+///// <summary>
+///// Describes a component manufacturedfrom processed materials.
+///// </summary>
+//type BasicComponent = {
+//    Name: string
+//    Materials: ProcessedMaterial list
+//}
+
+//type AssembledComponent = {
+//    Name: string
+//    SubComponents: BasicComponent list
+//}
+
+//type Component =
+//    | Basic of BasicComponent
+//    | Assembled of AssembledComponent
 
 
-type Product = {
-    Name: string
-    Components: Component list
-}
+//type Product = {
+//    Name: string
+//    Components: Component list
+//}
 
-type MaterialType =
-    | Metal
-    | Plastic
-    | Wood
-    | Composite
+//type MaterialType =
+//    | Metal
+//    | Plastic
+//    | Wood
+//    | Composite
 
-type MetalIngotType =
-    | IronIngot // of ProcessedMaterialType.MetalIngot
-    | SteelIngot //of ProcessedMaterialType.MetalIngot
+//type MetalIngotType =
+//    | IronIngot // of ProcessedMaterialType.MetalIngot
+//    | SteelIngot //of ProcessedMaterialType.MetalIngot
