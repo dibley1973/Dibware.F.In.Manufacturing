df08534a4e4f539aafe89c3731454df7a5adeb39
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-30 20:44:42 +0000

    Add `ProcessingPlants`

---
+++ src/Dibware.F.In.Manufacturing.Domain/Types/ProcessingPlants.fs
@@ -0,0 +1,7 @@
+namespace Dibware.F.In.Manufacturing.Domain.Types.ProcessingPlants
+
+/// <summary>
+/// Describes plant that Smelts ores into processed materials.
+/// </summary>
+type SmeltingPlant = 
+    | IronOreSmelter
