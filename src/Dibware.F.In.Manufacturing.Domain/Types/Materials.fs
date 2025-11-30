f79ef52e93ce925404b050cb50ff0d75741a96b0
Dibley1973 <dibley1973@yahoo.co.uk>
2025-11-30 21:02:51 +0000

    Clean up `Dibware.F.In.Manufacturing.Domain.Types.Materials`

--- src/Dibware.F.In.Manufacturing.Domain/Types/Materials.fs
+++ src/Dibware.F.In.Manufacturing.Domain/Types/Materials.fs
@@ -3,18 +3,11 @@ namespace Dibware.F.In.Manufacturing.Domain.Types.Materials
 open Dibware.F.In.Manufacturing.Domain.Types.Mining
 
 (*
-This is a modified version of my origianlal material type definitions, created
+This is a modified version of my original material type definitions, created
 from a suggestion by Brian Berns, and shared on Stack Overflow.
 Source - https://stackoverflow.com/a/79833562
 Posted by Brian Berns, modified by community. See post 'Timeline' for change history
 *)
-
-//type Ore =
-//    | IronOre
-//    | Coal
-//    | CopperOre
-//    | GoldOre
-//    | SilverOre
 
 type Oil = CrudeOil
 
