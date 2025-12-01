d72cb7cb6392b0d4c16563ec994227b8addfcf3b
Dibley1973 <dibley1973@yahoo.co.uk>
2025-12-01 05:40:30 +0000

    Add skeletal `AlloyingTests`

---
+++ src/Dibware.F.In.Manufacturing.Domain.UnitTests/BehaviourTests/OperationsTests/AlloyingTests.fs
@@ -0,0 +1,25 @@
+namespace Dibware.F.In.Manufacturing.Domain.UnitTests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+/// <summary>
+/// Tests for "Alloying" behaviour
+/// </summary>
+[<TestClass>]
+type AlloyingTests () =
+
+    [<TestMethod>]
+    member this.tryMakeSteelIngot_WhenGivenIncorrectMill_ReturnsNone () =
+        Assert.IsTrue(false);
+
+    [<TestMethod>]
+    member this.tryMakeSteelIngot_WhenGivenIncorrectRecipe_ReturnsNone () =
+        Assert.IsTrue(false);
+
+    [<TestMethod>]
+    member this.tryMakeSteelIngot_WhenGivenMateialList_ReturnsNone () =
+        Assert.IsTrue(false);
+
+    [<TestMethod>]
+    member this.tryMakeSteelIngot_WhenInputsAreCorrect_ReturnsSteelIngot() =
+        Assert.IsTrue(false);
