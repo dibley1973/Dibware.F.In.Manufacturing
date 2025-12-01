24fd998a90b77dc96a1d84f8c18a7161307acb55
Dibley1973 <dibley1973@yahoo.co.uk>
2025-12-01 05:59:07 +0000

    Add "meat" to the "bones" of `AlloyingTests`

--- src/Dibware.F.In.Manufacturing.Domain.UnitTests/BehaviourTests/OperationsTests/AlloyingTests.fs
+++ src/Dibware.F.In.Manufacturing.Domain.UnitTests/BehaviourTests/OperationsTests/AlloyingTests.fs
@@ -1,6 +1,12 @@
 namespace Dibware.F.In.Manufacturing.Domain.UnitTests
 
 open Microsoft.VisualStudio.TestTools.UnitTesting
+open Dibware.F.In.Manufacturing.Domain.Types.ProcessingPlants
+open Dibware.F.In.Manufacturing.Domain.Behaviours.Operations
+open Dibware.F.In.Manufacturing.Domain.Types.Recipies.Metals
+open Dibware.F.In.Manufacturing.Domain.Types.Manufacturing
+open Dibware.F.In.Manufacturing.Domain.Types.Materials
+open Dibware.F.In.Manufacturing.Domain.Types.Mining
 
 /// <summary>
 /// Tests for "Alloying" behaviour
@@ -8,18 +14,69 @@ open Microsoft.VisualStudio.TestTools.UnitTesting
 [<TestClass>]
 type AlloyingTests () =
 
+    let ironIngot = IronIngot |> RefinedIngot |> Refined
+    let coal = Ore.Coal |> RawOre |> Raw
+    let aluminaPowder = Powder.AluminaPowder |> RefinedPowder |> Refined
+    let causticSoda = Chemical.CausticSoda |> SynthesizedChemical |> Chemical
+
     [<TestMethod>]
     member this.tryMakeSteelIngot_WhenGivenIncorrectMill_ReturnsNone () =
-        Assert.IsTrue(false);
+        // Arrange
+        let incorrectMill = MillingPlant.AlluminiumMill
+        let recipe = Alloying.steelRecipe
+        let materialList = MaterialList [ (ironIngot, 2); (coal, 1) ]
+
+        // Act
+        let actual = Alloying.tryMakeSteelIngot incorrectMill recipe materialList
+
+        // Assert
+        Assert.IsTrue(actual.IsNone)
 
     [<TestMethod>]
     member this.tryMakeSteelIngot_WhenGivenIncorrectRecipe_ReturnsNone () =
-        Assert.IsTrue(false);
+        let millingPlant = MillingPlant.SteelMill
+        let incorrectRecipe = Alloying.aluminiumRecipe
+        let materialList = MaterialList [ (ironIngot, 2); (coal, 1) ]
+
+        // Act
+        let actual = Alloying.tryMakeSteelIngot millingPlant incorrectRecipe materialList
+
+        // Assert
+        Assert.IsTrue(actual.IsNone)
 
     [<TestMethod>]
-    member this.tryMakeSteelIngot_WhenGivenMateialList_ReturnsNone () =
-        Assert.IsTrue(false);
+    member this.tryMakeSteelIngot_WhenGivenIncorrectMateialList_ReturnsNone () =
+        let millingPlant = MillingPlant.SteelMill
+        let recipe = Alloying.steelRecipe
+        let incorrectMaterialList = MaterialList [ (aluminaPowder, 3); (causticSoda, 1) ]
+
+        // Act
+        let actual = Alloying.tryMakeSteelIngot millingPlant recipe incorrectMaterialList
+
+        // Assert
+        Assert.IsTrue(actual.IsNone)
+
+    [<TestMethod>]
+    member this.tryMakeSteelIngot_WhenGivenEmptyMateialList_ReturnsNone () =
+        let millingPlant = MillingPlant.SteelMill
+        let recipe = Alloying.steelRecipe
+        let emptyMaterialList = Map.empty
+
+        // Act
+        let actual = Alloying.tryMakeSteelIngot millingPlant recipe emptyMaterialList
+
+        // Assert
+        Assert.IsTrue(actual.IsNone)
 
     [<TestMethod>]
     member this.tryMakeSteelIngot_WhenInputsAreCorrect_ReturnsSteelIngot() =
-        Assert.IsTrue(false);
+        let millingPlant = MillingPlant.SteelMill
+        let recipe = Alloying.steelRecipe
+        let materialList = MaterialList [ (ironIngot, 2); (coal, 1) ]
+
+        // Act
+        let actual = Alloying.tryMakeSteelIngot millingPlant recipe materialList
+
+        // Assert
+        Assert.IsTrue(actual.IsSome)
+        Assert.AreEqual(Alloying.steelIngot, actual.Value)
